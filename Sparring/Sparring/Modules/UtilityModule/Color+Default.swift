5572e10f0b01c09364bef618b4eefdda71e0773d
wojciech <w.kulas2313@gmail.com>
2020-10-22 12:39:42 +0200

    FEAT: Created colors schemes.

---
+++ Sparring/Sparring/Modules/UtilityModule/Color+Default.swift
@@ -0,0 +1,36 @@
+//
+//  Color+Default.swift
+//  Sparring
+//
+//  Created by Wojciech Kulas on 22/10/2020.
+//
+
+import SwiftUI
+
+extension Color {
+    static let blueRibbon = Color("blueRibbon")
+    static let brightSun = Color("brightSun")
+    static let chatamsblue = Color("chatamsBlue")
+    static let congoBrown = Color("congoBrown")
+    static let coralRed = Color("coralRed")
+    static let deepOcean = Color("deepOcean")
+    static let ebonyClay = Color("ebonyClay")
+    static let ebonyClayLight = Color("ebonyClayLight")
+    static let fiord = Color("fiord")
+    static let gullGray = Color("gullGray")
+    static let kabul = Color("kabul")
+    static let mediumPurple = Color("mediumPurple")
+    static let mountainMeadow = Color("moutainMeadow")
+    static let mySin = Color("mySin")
+    static let neonCarrot = Color("neonCarrot")
+    static let oxfordBlue = Color("oxfordBlue")
+    static let oxfordBlueLight = Color("oxfordBlueLight")
+    static let persimmon = Color("persimmon")
+    static let plantation = Color("plantation")
+    static let rihno = Color("rihno")
+    static let royalBlue = Color("royalBlue")
+    static let shamrock = Color("shamrock")
+    static let shamrockLight = Color("shamrockLight")
+    static let verdigirs = Color("verdigris")
+    static let yellowOrange = Color("yellowOrange")
+}
--- Sparring/Sparring/SparringApp.swift
+++ Sparring/Sparring/SparringApp.swift
@@ -14,7 +14,7 @@ struct SparringApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                //.environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
