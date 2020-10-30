e52ba021e74c8ac2d5f1a0bb19ec73bb7242804f
wojciech <w.kulas2313@gmail.com>
2020-10-30 16:08:47 +0100

    REFACTOR

--- Sparring/SparringUITests/App Flow/Sign In/SignInUITests.swift
+++ Sparring/SparringUITests/App Flow/Sign In/SignInUITests.swift
@@ -8,23 +8,20 @@
 import XCTest
 
 class SignInUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        app = XCUIApplication()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
     func testCreateAccountButton_onTap_goToCreateAccountView() throws {
         // given
-        let app = XCUIApplication()
         let createAccountButton = app.buttons["Create an account"]
         let helloStaticText = app.staticTexts["Hello"]
         
